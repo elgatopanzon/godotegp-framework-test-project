64830f88c7c32343d968e964881128e03112180b
ElGatoPanzon <gato@elgatopanzon.io>
2023-11-08 11:50:00 -0600

    DataService: refactor short methods to create DataOperationProcess objects using Endpoints

--- addons/godotegp-framework-csharp/scripts/DataOperationProcess.cs
+++ addons/godotegp-framework-csharp/scripts/DataOperationProcess.cs
@@ -52,11 +52,19 @@ public class DataOperationProcessFile<T> : DataOperationProcess<T>
 	public DataOperationProcessFile(string filePath, object dataObject, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null) : base(new DataOperationFile<T>(new DataEndpointFile(filePath), dataObject), onWorkingCb, onProgressCb, onCompleteCb, onErrorCb)
 	{
 	}
+
+	public DataOperationProcessFile(DataEndpointFile fileEndpoint, object dataObject, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null) : base(new DataOperationFile<T>(fileEndpoint, dataObject), onWorkingCb, onProgressCb, onCompleteCb, onErrorCb)
+	{
+	}
 }
 
 public class DataOperationProcessHTTP<T> : DataOperationProcess<T>
 {
 	public DataOperationProcessHTTP(string hostname, int port = 443, string path = "/", Dictionary<string,object> urlParams = null, HttpMethod requestMethod = null, bool verifySSL = true, int timeout = 30, Dictionary<string, string> headers = null, object dataObject = null, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null) : base(new DataOperationHTTP<T>(new DataEndpointHTTP(hostname, port, path, urlParams, requestMethod, verifySSL, timeout, headers), dataObject), onWorkingCb, onProgressCb, onCompleteCb, onErrorCb)
+	{
+	}
+
+	public DataOperationProcessHTTP(DataEndpointHTTP httpEndpoint, object dataObject = null, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null) : base(new DataOperationHTTP<T>(httpEndpoint, dataObject), onWorkingCb, onProgressCb, onCompleteCb, onErrorCb)
 	{
 	}
 }
--- addons/godotegp-framework-csharp/scripts/services/DataService.cs
+++ addons/godotegp-framework-csharp/scripts/services/DataService.cs
@@ -10,7 +10,7 @@ public partial class DataService : Service
 	// load/save to/from file endpoint
 	public DataOperation LoadFromFile<T>(string filePath, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null)
 	{
-		DataOperationProcessFile<T> dopf = new DataOperationProcessFile<T>(filePath, null, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
+		var dopf = DataOperationFromEndpoint<T>(new DataEndpointFile(filePath), null, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
 		dopf.Load();
 
 		return dopf.DataOperation;
@@ -18,7 +18,7 @@ public partial class DataService : Service
 
 	public DataOperation SaveToFile<T>(string filePath, object dataObject, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null)
 	{
-		DataOperationProcessFile<T> dopf = new DataOperationProcessFile<T>(filePath, dataObject, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
+		var dopf = DataOperationFromEndpoint<T>(new DataEndpointFile(filePath), null, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
 		dopf.Save();
 
 		return dopf.DataOperation;
@@ -26,9 +26,29 @@ public partial class DataService : Service
 
 	public DataOperation HTTPRequest<T>(string hostname, int port = 443, string path = "/", Dictionary<string,object> urlParams = null, HttpMethod requestMethod = null, bool verifySSL = true, int timeout = 30, Dictionary<string, string> headers = null, object dataObject = null, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null)
 	{
-		DataOperationProcessHTTP<T> dataOperationProcessHTTP = new DataOperationProcessHTTP<T>(hostname, port, path, urlParams, requestMethod, verifySSL, timeout, headers, dataObject, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
-		dataOperationProcessHTTP.Save();
+		var doph = DataOperationFromEndpoint<T>(new DataEndpointHTTP(hostname, port, path, urlParams, requestMethod, verifySSL, timeout, headers), dataObject, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
+		doph.Save();
 
-		return dataOperationProcessHTTP.DataOperation;
+		return doph.DataOperation;
+	}
+
+	public DataOperationProcess<T> DataOperationFromEndpoint<T>(IDataEndpointObject dataEndpointObject, object dataObject, Action<IEvent> onWorkingCb = null, Action<IEvent> onProgressCb = null, Action<IEvent> onCompleteCb = null, Action<IEvent> onErrorCb = null)
+	{
+		if (dataEndpointObject is DataEndpointFile def)
+		{
+			DataOperationProcessFile<T> dopf = new DataOperationProcessFile<T>(def, dataObject, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
+
+			return dopf;
+		}
+		else if (dataEndpointObject is DataEndpointHTTP deh)
+		{
+			DataOperationProcessHTTP<T> dopf = new DataOperationProcessHTTP<T>(deh, dataObject, onWorkingCb, onProgressCb, onCompleteCb, onErrorCb);
+
+			return dopf;
+		}
+		else
+		{
+			throw new NotImplementedException($"{dataEndpointObject.GetType().Name} type is not implemented!");
+		}
 	}
 }
