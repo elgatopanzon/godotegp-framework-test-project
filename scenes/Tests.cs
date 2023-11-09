2e455b2d9cd6063c7e5d06d4d941baf231436f69
ElGatoPanzon <gato@elgatopanzon.io>
2023-11-09 16:43:12 -0600

    Tests scene: implement basic event handler for save/load buttons

--- scenes/Tests.cs
+++ scenes/Tests.cs
@@ -1,15 +1,32 @@
 using Godot;
 using System;
 
+using GodotEGP;
+using GodotEGP.Logging;
+using GodotEGP.Service;
+using GodotEGP.Objects.Extensions;
+using GodotEGP.Event.Events;
+
 public partial class Tests : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		ServiceRegistry.Get<NodeManager>().SubscribeSignal("UITests.Save.Save", "pressed", false, _On_SaveTest_Save_pressed, isHighPriority: true);
+		ServiceRegistry.Get<NodeManager>().SubscribeSignal("UITests.Save.Load", "pressed", false, _On_SaveTest_Load_pressed, isHighPriority: true);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void _On_SaveTest_Save_pressed(IEvent e)
+	{
+		LoggerManager.LogDebug("Save pressed", "", "event", e);
+	}
+	public void _On_SaveTest_Load_pressed(IEvent e)
+	{
+		LoggerManager.LogDebug("Load pressed", "", "event", e);
 	}
 }
