25531ccc1d8e88d656daa1a1e3da3cd554b61899
ElGatoPanzon <gato@elgatopanzon.io>
2024-05-12 19:35:03 -0600

    Profiling: setup profiling for ECSv4

--- profiling/cli/Program.cs
+++ profiling/cli/Program.cs
@@ -15,6 +15,7 @@ using GodotEGP.Event.Filters;
 using Godot;
 
 using GodotEGP.Profiling.CLI.ECSv3;
+using GodotEGP.Profiling.CLI.ECSv4;
 
 internal class Program
 {
@@ -118,6 +119,73 @@ internal class Program
 						break;
 					case "update_2m":
 						profile = new ECSv3Profile_Update_6(2000000) {
+						};
+						break;
+    				default:
+    					break;
+    			}
+    			break;
+    		case "ECSv4":
+    			switch (profileSubName)
+    			{
+					case "update_1":
+						profile = new ECSv4Profile_Update_6(1) {
+						};
+						break;
+					case "update_2":
+						profile = new ECSv4Profile_Update_6(2) {
+						};
+						break;
+					case "update_8":
+						profile = new ECSv4Profile_Update_6(8) {
+						};
+						break;
+					case "update_16":
+						profile = new ECSv4Profile_Update_6(16) {
+						};
+						break;
+					case "update_32":
+						profile = new ECSv4Profile_Update_6(32) {
+						};
+						break;
+					case "update_64":
+						profile = new ECSv4Profile_Update_6(64) {
+						};
+						break;
+					case "update_128":
+						profile = new ECSv4Profile_Update_6(128) {
+						};
+						break;
+					case "update_256":
+						profile = new ECSv4Profile_Update_6(256) {
+						};
+						break;
+					case "update_1k":
+						profile = new ECSv4Profile_Update_6(1000) {
+						};
+						break;
+					case "update_4k":
+						profile = new ECSv4Profile_Update_6(4000) {
+						};
+						break;
+					case "update_16k":
+						profile = new ECSv4Profile_Update_6(16000) {
+						};
+						break;
+					case "update_65k":
+						profile = new ECSv4Profile_Update_6(65000) {
+						};
+						break;
+					case "update_262k":
+						profile = new ECSv4Profile_Update_6(262000) {
+						};
+						break;
+					case "update_1m":
+						profile = new ECSv4Profile_Update_6(1000000) {
+						};
+						break;
+					case "update_2m":
+						profile = new ECSv4Profile_Update_6(2000000) {
 						};
 						break;
     				default:
